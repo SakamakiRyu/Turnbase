55668e5afc794ca0f9e020c4dfe2fd6d354022a8
SakamakiRyu <potesara031w@gmail.com>
2022-09-12 11:32:49 +0900

    PRC関数の修正

--- Assets/Scripts/EventTest.cs
+++ Assets/Scripts/EventTest.cs
@@ -22,18 +22,20 @@ public class EventTest : MonoBehaviour
         _tiles = FindObjectsOfType<Tile>();
     }
 
+    // タイルがクリックされた時に呼ばれる
     public void OnPointerClick(Tile tile)
     {
         var id = tile.ID;
-        _photonView.RPC(nameof(SendDate), RpcTarget.All, id);
+        var actNum = PhotonNetwork.LocalPlayer.ActorNumber;
+        _photonView.RPC(nameof(SendDate), RpcTarget.All,actNum, id);
     }
 
     [PunRPC]
-    public void SendDate(int ID)
+    public void SendDate(int actorNumber,int ID)
     {
         var targetTile = _tiles.Where(x => x.ID == ID).First(x => x.GetComponent<Tile>());
 
-        if (PhotonNetwork.LocalPlayer.ActorNumber == 1)
+        if (actorNumber == 1)
         {
             targetTile.SetColor(Color.red);
         }
